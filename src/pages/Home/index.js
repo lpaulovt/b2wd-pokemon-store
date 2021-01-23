8df022ce0933a7c7bf78726d92c2d7ec46a97a9b
lpaulovt <silvavitorlopes711@gmail.com>
2021-01-23 01:28:42 -0300

    :sparkles: Creating button component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import api from "../../services/api";
+import Card from "../../components/Card/index";
 import Button from "../../components/Button";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
@@ -87,59 +88,3 @@ const Home = () => {
 };
 
 export default Home;
-
-const Card = ({ data, addCart }) => {
-  const url = data.url;
-  const [pokemon, setPokemon] = useState(null);
-
-  useEffect(() => {
-    api
-      .get(`pokemon/${url.substring(33)}`)
-      .then((response) => {
-        setPokemon(response.data);
-      })
-      .catch((error) => console.log(error));
-  }, [url]);
-  console.log(pokemon);
-
-  function handleClick() {
-    let data = {
-      id: pokemon.id,
-      name: pokemon.name,
-      img: pokemon.sprites.other["official-artwork"].front_default,
-    };
-
-    addCart(data);
-  }
-
-  return (
-    <>
-      {pokemon === null ? null : (
-        <div className="card">
-          <img
-            src={pokemon.sprites.other["official-artwork"].front_default}
-            alt=""
-            className="card-image"
-          />
-          <div className="card-info">
-            <h2>{pokemon.name}</h2>
-            <div class="types">
-              {pokemon.types.map((type) => (
-                <span>{type.type.name}</span>
-              ))}
-            </div>
-
-            <h3>R$2000,00</h3>
-          </div>
-          <div class="card-overlay">
-            <Button
-              label="Adicionar ao carrinho"
-              onClick={handleClick}
-              type="primmary"
-            />
-          </div>
-        </div>
-      )}
-    </>
-  );
-};
